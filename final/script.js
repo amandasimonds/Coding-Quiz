7d743b4f4c17af3df31925c99c94881275a817b5
Amanda Tree <amandasimonds9@gmail.com>
2019-10-06 14:04:08 -0600

    elements created

---
+++ final/script.js
@@ -0,0 +1,13 @@
+v
+
+var startElem = document.getElementById("start");
+var quizElem = document.getElementById("quiz");
+var questionElem = document.getElementById("question");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+var counterElem = document.getElementById("counter");
+var timeGaugeElem = document.getElementById("timeGauge");
+var progressElem = document.getElementById("progress");
+var scoreDiv = document.getElementById("score");
